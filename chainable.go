de48a154096fd184a73f5a13f463f0beade9fbd0
Mauricio Klein <mauricio.klein.msk@gmail.com>
2018-03-04 16:51:41 +0100

    Add support to ChainDummy (i.e. without error checking)

--- chainable.go
+++ chainable.go
@@ -10,41 +10,56 @@ var (
 	errArgumentMismatch = errors.New("fn argument mismatch")
 )
 
-// Chain defines a new chain structure
-type Chain struct {
+// Chainable defines a new chainable structure
+type Chainable struct {
 	from  []interface{}
-	funcs []interface{}
+	links []Link
 }
 
-// New instantiate a new chain instance
-func New() *Chain {
-	return &Chain{}
+// Link represents a function in a chain
+type Link struct {
+	fn          interface{}
+	handleError bool
+}
+
+// New instantiate a new chainable instance
+func New() *Chainable {
+	return &Chainable{}
 }
 
 // From defines the arguments provided to the first
 // link in a chain
-func (c *Chain) From(args ...interface{}) *Chain {
+func (c *Chainable) From(args ...interface{}) *Chainable {
 	c.from = args
 	return c
 }
 
-// Link appends a new function to the chain
-func (c *Chain) Link(funcs ...interface{}) *Chain {
+// Chain appends a new function to the chain, with error
+// checking enabled
+func (c *Chainable) Chain(funcs ...interface{}) *Chainable {
 	for _, fn := range funcs {
-		c.funcs = append(c.funcs, fn)
+		c.addFunc(fn, true)
 	}
+	return c
+}
 
+// ChainDummy appends a new function to the chain, with error
+// checking disabled
+func (c *Chainable) ChainDummy(funcs ...interface{}) *Chainable {
+	for _, fn := range funcs {
+		c.addFunc(fn, false)
+	}
 	return c
 }
 
 // Unwrap process a chain, returning the result and error
 // of the last execution (success or error)
-func (c *Chain) Unwrap() ([]interface{}, error) {
+func (c *Chainable) Unwrap() ([]interface{}, error) {
 	v := c.from
 	var err error
 
-	for _, fn := range c.funcs {
-		if v, err = callFn(fn, v); err != nil {
+	for _, link := range c.links {
+		if v, err = processLink(link, v); err != nil {
 			return nil, err
 		}
 	}
@@ -52,14 +67,21 @@ func (c *Chain) Unwrap() ([]interface{}, error) {
 	return v, nil
 }
 
-// callFn calls the function fn, transforming args
-// using reflection
-func callFn(fn interface{}, args []interface{}) ([]interface{}, error) {
-	vfn := reflect.ValueOf(fn)
+// addFunc add a new function to the chain, creating the underlying link
+func (c *Chainable) addFunc(fn interface{}, handleError bool) {
+	c.links = append(c.links, Link{
+		fn:          fn,
+		handleError: handleError,
+	})
+}
+
+// processLink calls the function fn associated to the link, transforming args using reflection
+func processLink(link Link, args []interface{}) ([]interface{}, error) {
+	vfn := reflect.ValueOf(link.fn)
 	vfnType := vfn.Type()
 
 	// check if it's a function
-	if vfn.Type().Kind() != reflect.Func {
+	if vfnType.Kind() != reflect.Func {
 		return nil, errNotAFunction
 	}
 
@@ -68,19 +90,16 @@ func callFn(fn interface{}, args []interface{}) ([]interface{}, error) {
 		return nil, errArgumentMismatch
 	}
 
-	// build the reflected args
-	reflectedArgs := buildReflectedArgs(args)
-
 	// call the function
 	out := []interface{}{}
-	for _, o := range vfn.Call(reflectedArgs) {
+	for _, o := range vfn.Call(reflectArgs(args)) {
 		out = append(out, o.Interface())
 	}
 
 	// if the last returned value for the function
 	// is an error, cast the error and return it
-	// along with the rest
-	if doesReturnError(vfnType) {
+	// along with the rest of values
+	if link.handleError && doesReturnError(vfnType) {
 		err, _ := out[len(out)-1].(error)
 		return out[:len(out)-1], err
 	}
@@ -88,9 +107,9 @@ func callFn(fn interface{}, args []interface{}) ([]interface{}, error) {
 	return out, nil
 }
 
-// buildReflectedArgs transforms the args list in a list of
+// reflectArgs transforms the args list in a list of
 // reflect.Value, used to call a function using reflection
-func buildReflectedArgs(args []interface{}) []reflect.Value {
+func reflectArgs(args []interface{}) []reflect.Value {
 	in := make([]reflect.Value, len(args))
 
 	for k, arg := range args {
--- chainable_test.go
+++ chainable_test.go
@@ -1,41 +1,46 @@
 package chainable
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestChain_Success(t *testing.T) {
-	sum2 := func(x int) (int, error) { return x + 2, nil }
-	mul2 := func(x int) (int, error) { return x * 2, nil }
+func TestChain(t *testing.T) {
+	sum2 := func(x int) int { return x + 2 }
+	mul2 := func(x int) int { return x * 2 }
 
 	// (4 + 2) * 2 = 12
 	ret, err := New().
 		From(4).
-		Link(sum2, mul2).
+		Chain(sum2, mul2).
 		Unwrap()
 
 	assert.Equal(t, []interface{}{12}, ret)
 	assert.NoError(t, err)
 }
 
-func TestChain_NotAFunction(t *testing.T) {
-	_, err := New().Link("not a function").Unwrap()
+func TestChainDummy(t *testing.T) {
+	genericError := errors.New("a generic error")
+
+	f1 := func() error { return genericError }
+	f2 := func(e error) int { return 0 }
+
+	_, err := New().ChainDummy(f1, f2).Unwrap()
+
+	assert.NoError(t, err)
+}
+
+func TestNotAFunction(t *testing.T) {
+	_, err := New().Chain("not a function").Unwrap()
 	assert.EqualError(t, err, errNotAFunction.Error())
 }
 
-func TestChain_ArgumentMismatch(t *testing.T) {
+func TestArgumentMismatch(t *testing.T) {
 	f1 := func() (int, error) { return 0, nil }
 	f2 := func(a, b int) (int, error) { return 0, nil }
 
-	_, err := New().Link(f1, f2).Unwrap()
+	_, err := New().Chain(f1, f2).Unwrap()
 	assert.EqualError(t, err, errArgumentMismatch.Error())
-}
-
-func TestChain_AcceptFunctionNotReturningError(t *testing.T) {
-	f1 := func() int { return 0 }
-
-	_, err := New().Link(f1).Unwrap()
-	assert.NoError(t, err)
 }
