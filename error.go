e48008c986bd5e5fb6eb6c6c1b5effa040287c60
Mauricio Klein <mauricio.klein.msk@gmail.com>
2018-03-08 18:44:22 +0100

    Code refactoring (#4)
    - Adjust addFuncs to receive a slice of functions instead of a single one
    - Refactor errors, transforming them in simple functions instead complex structures

--- error.go
+++ error.go
@@ -4,41 +4,15 @@ import (
 	"fmt"
 )
 
-// NotAFunctionError defines the error raised
-// when a non-function object is chained
-type NotAFunctionError struct {
-	linkIndex int
+// notAFunctionError returns the formatted error
+// when an element in the chain isn't a function
+func notAFunctionError(linkIndex int) error {
+	return fmt.Errorf("(Error on Link: %d) Element isn't a function", linkIndex)
 }
 
-// ArgumentMismatchError defines the error raised
-// when the number of arguments provided to a function
-// doesn't match the function arity
-type ArgumentMismatchError struct {
-	linkIndex int
-	nArgs     int
-	fnArity   int
-}
-
-// NewNotAFunctionError creates a new instance of NotAFunctionError
-func NewNotAFunctionError(linkIndex int) *NotAFunctionError {
-	return &NotAFunctionError{linkIndex}
-}
-
-// NewArgumentMismatchError creates a new instance of ArgumentMismatchError
-func NewArgumentMismatchError(linkIndex, nArgs, fnArity int) *ArgumentMismatchError {
-	return &ArgumentMismatchError{
-		linkIndex: linkIndex,
-		nArgs:     nArgs,
-		fnArity:   fnArity,
-	}
-}
-
-// Error provides the description of the error
-func (err *NotAFunctionError) Error() string {
-	return fmt.Sprintf("(Error on Link: %d) Element isn't a function", err.linkIndex)
-}
-
-// Error provides the description of the error
-func (err *ArgumentMismatchError) Error() string {
-	return fmt.Sprintf("(Error on Link: %d) %d arg(s) provided, but function arity is %d", err.linkIndex, err.nArgs, err.fnArity)
+// argumentMismatchError returns the formatted error
+// when the number of arguments provided doesn't match
+// the function's arity
+func argumentMismatchError(linkIndex, nArgs, fnArity int) error {
+	return fmt.Errorf("(Error on Link: %d) %d arg(s) provided, but function arity is %d", linkIndex, nArgs, fnArity)
 }
